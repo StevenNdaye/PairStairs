468d8bfb1108acf86d7eccbe1b583510e5cc7fba
Wole Solana <osolana@thoughtworks.com>
2013-06-19 14:50:54 +0200

    Textbox to input data and refresh data automatically

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -3,15 +3,46 @@ developer.controllers = developer.controllers || {};
 
 developer.controllers.index = function(){
     var initialize = function () {
-        $.getJSON("/PairStairs/api/developers").success(
-            function(data){
-                data.forEach(
-                    function(dev){
-                        $("body").append("<div>DEV: "+ dev.name + " "+ dev.id + "</div>")
+
+        var viewModel = {
+            devs: ko.observableArray(),
+            count: ko.observable(),
+            newName: ko.observable(),
+            submit: function(){
+                $.ajax({
+                    url: "/PairStairs/api/developers/new",
+                    type: "POST",
+                    contentType: "application/json",
+                    data: JSON.stringify({name: this.newName()})
+                }).success(function(){
+                    viewModel.listDevelopers();
+                })
+            },
+
+            listDevelopers: function(){
+                $.ajax({
+                    url: "/PairStairs/api/developers",
+                    type: "GET"
+                }).success(
+                    function(data){
+                       viewModel.devs(data);
                     }
-            );
-        });
-    }
+                );
+
+                $.getJSON("/PairStairs/api/developers/count").success(
+                        function(count){
+                           viewModel.count(count);
+                        }
+                    );
+                }
+
+        };
+
+        viewModel.listDevelopers();
+
+        ko.applyBindings(viewModel)
+    };
+
     return {
         initialize:initialize
     };
