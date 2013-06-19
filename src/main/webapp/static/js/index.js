ffcf20cb776c7e0843b7e651e480eda49da737f8
Wole Solana <osolana@thoughtworks.com>
2013-06-19 10:19:32 +0200

    Changed path from resteasy to PairStairs

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -3,7 +3,7 @@ locomotives.controllers = locomotives.controllers || {};
 
 locomotives.controllers.index = function(){
     var initialize = function () {
-        $.getJSON("/resteasy-hey-world/api/locomotives").success(
+        $.getJSON("/PairStairs/api/locomotives").success(
             function(data){
                 data.forEach(
                     function(train){
