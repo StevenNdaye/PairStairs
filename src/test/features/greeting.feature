eca4a80a6e3518d1af5cbfeeb343ea89f7108efa
Wole Solana <osolana@thoughtworks.com>
2013-06-20 16:47:12 +0200

    added tests for viewing, creating and deleting data

--- src/test/features/greeting.feature
+++ src/test/features/greeting.feature
@@ -2,3 +2,21 @@ Feature: Greetings
   Scenario: Users should be greeted
     Given I visit the main page
     Then I should be greeted with "Whaddup Yo!"
+
+  Scenario: Should see list of developers
+    Given I visit the main page
+    Then I should see id "1", name "STEVEN"
+    And I should see id "2", name "MUSA"
+    And I should see id "3", name "`WOLE"
+
+  Scenario: Should add new developer
+    Given I visit the main page
+    When I enter a developer name as "CHRIS"
+    And I click on the "Create" button
+    Then I should see id "4", name "CHRIS"
+
+  Scenario: Should delete existing developer
+    Given I visit the main page
+    When I enter developer ID "1" in the Developer ID textbox
+    And I click on the "Delete" button
+    Then Developer with ID "1" should be deleted from the list of developers
--- src/test/features/step_definitions/main_page_steps.rb
+++ src/test/features/step_definitions/main_page_steps.rb
@@ -6,3 +6,47 @@ Then /^I should be greeted with "([^"]*)"$/ do |expected_greeting|
   greeting = @driver.find_element(:tag_name, "h1")
   greeting.text.should eq(expected_greeting)
 end
+
+When /^I enter a developer name as "([^"]*)"$/ do |expected_developer|
+   new_developer = @driver.find_element(:id, "createDeveloperNameTextBox")
+   new_developer.send_keys(expected_developer)
+end
+
+And /^I click on the "Create" button$/ do
+    create_button =  @driver.find_element(:id, "createDeveloperNameButton")
+    create_button.click
+end
+
+Then /^I should see id "([^"]*)", name "([^"]*)"$/ do |expected_id, expected_developer|
+    wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+    wait.until {@driver.find_element(:id, "developer_#{expected_id}") }
+
+    table_row = @driver.find_element(:id, "developer_#{expected_id}")
+
+    id = table_row.find_element(:class_name, "id")
+    name = table_row.find_element(:class_name, "name")
+    id.text.should eq(expected_id)
+    name.text.should eq(expected_developer)
+end
+
+When /^I enter developer ID "([^"]*)" in the Developer ID textbox$/ do |expected_id|
+   current_id = @driver.find_element(:id, "deleteDeveloperIdTextBox")
+   current_id.send_keys(expected_id)
+end
+
+And /^I click on the "Delete" button$/ do
+    delete_button =  @driver.find_element(:id, "deleteDeveloperIdButton")
+    delete_button.click
+end
+
+Then /^Developer with ID "([^"]*)" should be deleted from the list of developers$/ do |expected_id|
+   wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+   wait.until {
+        begin
+            current_id = @driver.find_element(:id, "developer_#{expected_id}")
+            false
+        rescue Selenium::WebDriver::Error::NoSuchElementError
+            true
+        end
+   }
+end
